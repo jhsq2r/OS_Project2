c4e7165ff11a2407641b6fd9e4b49db553f825b2
jhsq2r <126211836+jhsq2r@users.noreply.github.com>
2023-09-16 12:44:21 -0500

    Update PCB oss.c

--- oss.c
+++ oss.c
@@ -5,9 +5,10 @@
 #include <sys/wait.h>
 #include <sys/shm.h>
 #include <sys/ipc.h>
+#include <time.h>
 
 //Creator: Jarod Stagner
-//Turn-in date: 
+//Turn-in date:
 
 #define SHMKEY 55555
 
@@ -16,22 +17,18 @@ struct PCB {
         pid_t pid;
         int startSeconds;
         int startNano;
-}
+};
 
-void launchChild(){
-        pid_t child_pid = fork();
-        if(child_pid == 0){
-                //Make struct entry
-                //Make Random Second and NanoSecond based on parameters
-                char *args[] = {"./worker", "RandomSec", "RandomNano",NULL};
-                execvp("./worker", args);
+void displayTable(int i, struct PCB *processTable){
+        printf("Process Table:\nEntry Occupied PID StartS StartN\n");
+        for (int x = 0; x < i; x++){
 
-                printf("Something horrible happened...\n");
-                exit(1);
+                printf("%d      %d      %d      %d      %d\n", x,processTable[x].occupied,processTable[x].pid,processTable[x].startSeconds,processTable[x].startNano);
+
         }
 }
 
-void updateTime(){
+void updateTime(int *sharedTime){
         sharedTime[1] = sharedTime[1] + 100000000;
         if (sharedTime[1] >= 1000000000 ){
                 sharedTime[0] = sharedTime[0] + 1;
@@ -48,7 +45,7 @@ int main(int argc, char** argv) {
 
         int proc = 5;
         int simul = 3;
-        int time = 3;//default parameters
+        int maxTime = 3;//default parameters
 
         int shmid = shmget(SHMKEY, sizeof(int)*2, 0777 | IPC_CREAT);
         if(shmid == -1){
@@ -56,11 +53,11 @@ int main(int argc, char** argv) {
                 return EXIT_FAILURE;
         }
         int * sharedTime = (int *) (shmat (shmid, 0, 0));
-        sharedTime[0] = 5;
-        sharedTime[1] = 999000000;
+        sharedTime[0] = 0;
+        sharedTime[1] = 0;
 
         int option;
-               while((option = getopt(argc, argv, "hn:s:t:")) != -1) {//Read command line arguments
+        while((option = getopt(argc, argv, "hn:s:t:")) != -1) {//Read command line arguments
                 switch(option){
                         case 'h':
                                 help();
@@ -73,7 +70,7 @@ int main(int argc, char** argv) {
                                 simul = atoi(optarg);
                                 break;
                         case 't':
-                                time = atoi(optarg);
+                                maxTime = atoi(optarg);
                                 break;
                         case '?':
                                 help();
@@ -83,25 +80,40 @@ int main(int argc, char** argv) {
         }
 
         struct PCB processTable[20];
-
+        int status;
         int i = 0;
-        while(i < procs){
-                updateTime();
+        while(i < proc){
+                srand(time(NULL));
+                int pid = waitpid(-1,&status,WNOHANG);
+                //printf("i = %d\n", i);
+                updateTime(sharedTime);
                 if (sharedTime[1] == 500000000){
-                        //display table
-                {
+                        displayTable(i, processTable);
+                }
 
-                if (i >= simul){
-                        int pid = waitpid(-1,0,WNOHANG);
-                        if(pid == 0){
-                                //Nothing
+                if((i >= simul && pid != 0) || i < simul){
+                        pid_t child_pid = fork();
+                        if(child_pid == 0){
+                                char convertSec[20];
+                                char convertNan[20];
+
+                                int randomSec = (rand() % ((maxTime - 1) -1 + 1)) + 1;
+                                int randomNano = (rand() % (999999999 - 1 + 1)) + 1;
+
+                                sprintf(convertSec, "%d", randomSec);
+                                sprintf(convertNan, "%d", randomNano);
+
+                                char *args[] = {"./worker", convertSec, convertNan, NULL};
+                                execvp("./worker", args);
+
+                                printf("Something horrible happened...\n");
+                                exit(1);
                         }else{
-                                launchChild();
-                                i++;
+                                processTable[i].occupied = 1;
+                                processTable[i].pid = child_pid;
+                                processTable[i].startSeconds = sharedTime[0];
+                                processTable[i].startNano = sharedTime[1];
                         }
-
-                }else{
-                        launchChild();
                         i++;
                 }
 
@@ -118,4 +130,5 @@ int main(int argc, char** argv) {
 
         return 0;
 }
+
 
