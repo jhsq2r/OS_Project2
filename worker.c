000ce51ef018bbe9f2770a2c7ccf936eafe74dca
jhsq2r <126211836+jhsq2r@users.noreply.github.com>
2023-09-17 11:58:59 -0500

    Update worker.c

--- worker.c
+++ worker.c
@@ -14,10 +14,32 @@ int main(int argc, char** argv){
         }
         int * sharedTime = (int*) (shmat (shmid, 0, 0));
 
-        printf("This is Child: %d, From Parent: %d, Seconds: %d, NanoSeconds: %d\n", getpid(), getppid(), sharedTime[0], sharedTime[1]);
+        //printf("This is Child: %d, From Parent: %d, Seconds: %s, NanoSeconds: %s\n", getpid(), getppid(), argv[1], argv[2]);
+
+        int exitTime[2];
+        exitTime[0] = atoi(argv[1]) + sharedTime[0];
+        exitTime[1] = atoi(argv[2]) + sharedTime[1];
+        if (exitTime[1] >= 1000000000){
+                exitTime[1] -= 1000000000;
+                exitTime[0] += 1;
+        }
+
+        printf("WORKER PID:%d PPID:%d SysClockS: %d SysclockNano: %d TermTimeS: %d TermTimeNano: %d JUST STARTING\n",getpid(),getppid(),sharedTime[0],sharedTime[1],exitTime[0],exitTime[1]);
+
+        int currentTime = sharedTime[0];
+
+        while (exitTime[0] > sharedTime[0] || exitTime[1] > sharedTime[1]){
+                if(currentTime != sharedTime[0]){
+                        printf("WORKER PID:%d PPID:%d SysClockS: %d SysclockNano: %d TermTimeS: %d TermTimeNano: %d SECONDS HAVE PASSED\n",getpid(),getppid(),sharedTime[0],sharedTime[1],exitTime[0],exitTime[1]);
+                        currentTime = sharedTime[0];
+                }
+        }
+
+        printf("WORKER PID:%d PPID:%d SysClockS: %d SysclockNano: %d TermTimeS: %d TermTimeNano: %d TERMINATING\n",getpid(),getppid(),sharedTime[0],sharedTime[1],exitTime[0],exitTime[1]);
 
         shmdt(sharedTime);
 
         return 0;
 
 }
+
